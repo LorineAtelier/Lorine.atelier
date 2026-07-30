68f2df3fddada64718302a9dc7168e5ecff9b1d3
LorineAtelier <lorine.atelier@gmail.com>
2026-07-30 21:43:26 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".tab-content");
 
+tabs.forEach(tab => {
+
+    tab.addEventListener("click", () => {
+
+        tabs.forEach(t => t.classList.remove("active"));
+        contents.forEach(c => c.classList.remove("active"));
+
+        tab.classList.add("active");
+
+        const target = document.getElementById(tab.dataset.target);
+
+        target.classList.add("active");
+
+    });
+
+});
