8dacb391799bc3d0484cba2cc708b96de8b56a95
LorineAtelier <lorine.atelier@gmail.com>
2026-07-31 10:01:36 +0200

    Add cart management functions with localStorage

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,7 @@
+// Récupère le panier existant ou en crée un vide
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+// Fonction pour enregistrer le panier
+function saveCart() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
