d7e8480db44b9da55ca6ed6b24f4006c1b846dd7
LorineAtelier <lorine.atelier@gmail.com>
2026-07-31 10:01:59 +0200

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -5,3 +5,41 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 function saveCart() {
     localStorage.setItem("cart", JSON.stringify(cart));
 }
+
+const addButton = document.getElementById("add-to-cart");
+
+if (addButton) {
+
+    addButton.addEventListener("click", () => {
+
+        const title = document.querySelector(".product-info h1").textContent;
+
+        let type = "Original";
+        let format = "";
+        let price = 45;
+
+        if (document.getElementById("reproduction").classList.contains("active")) {
+
+            type = "Reproduction";
+
+            const select = document.getElementById("format");
+
+            format = select.options[select.selectedIndex].text;
+
+            price = Number(select.value);
+        }
+
+        cart.push({
+            title,
+            type,
+            format,
+            price
+        });
+
+        saveCart();
+
+        window.location.href = "panier.html";
+
+    });
+
+}
